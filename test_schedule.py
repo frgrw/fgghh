13837a0b99c813aa449eac7064c3fad4dab239e6
Christopher Berner <christopherberner@gmail.com>
2019-12-17 21:00:23 -0800

    Fix schedule parsing test

--- test_schedule.py
+++ test_schedule.py
@@ -15,13 +15,12 @@ class ScheduleTestCase(unittest.TestCase):
             self.assertFalse(check_schedule(start, event_29_days_later + datetime.timedelta(days=i), 29))
         self.assertTrue(check_schedule(start, event_29_days_later + datetime.timedelta(days=29), 29))
 
-    def schedule_parsing(self):
-        self.assertEqual(parse_schedule("0 * * * *", "UTC"), ("0 * * * *", {}))
+    def test_schedule_parsing(self):
+        self.assertEqual(parse_schedule("0 * * * *"), ("0 * * * *", {}))
 
-        cron, schedule = parse_schedule("starting  Jan 1 2019 every 11  days at 13:00", "America/Los_Angeles")
+        cron, schedule = parse_schedule("starting  Jan 1 2019 every 11  days at 13:00")
         self.assertEqual(cron, "0 13 * * *")
-        start = parser.parse("Jan 1 2019").replace(tzinfo=tz.gettz("America/Los_Angeles"))
-        self.assertEqual(schedule, {'start': start, 'frequency': 11, 'unit': 'day'})
+        self.assertEqual(schedule, {'start': '2019-01-01', 'frequency': 11, 'unit': 'day'})
 
 
 if __name__ == '__main__':
